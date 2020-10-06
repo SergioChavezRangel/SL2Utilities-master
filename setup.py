f409e7f8f53ec5e49a9bfbf98cac047a272f6951
Sergio Chávez <malditoduendeherido@hotmail.com>
2020-10-06 10:57:29 -0500

    Project packaged at dist

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name='sl2util',
     version='1.0.0',
-    packages=[],
+    packages=['sl2util'],
     url='',
     license='MIT License',
     author='SergioChavez',
     author_email='l2.steel.automation@gmail.com',
     description='Steelmaking Automation Utils',
     long_description=open('README.md').read(),
-    py_modules=['logger', 'configdatareader', ],
+    py_modules=['sl2util.logger', 'sl2util.configdatareader', ],
 )
